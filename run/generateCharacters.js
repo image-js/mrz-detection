4cab5ae16013955f3b52039ac42473f678d10a52
Daniel Kostro <kostro.d@gmail.com>
2018-03-22 07:46:45 +0100

    generate chars

--- run/generateCharacters.js
+++ run/generateCharacters.js
@@ -7,6 +7,8 @@ const { generateSymbolImage, getLinesFromImage } = require('ocr-tools');
 const minimist = require('minimist');
 
 const argv = minimist(process.argv.slice(2));
+const roiOptions = require('../src/roiOptions');
+const { writeImages } = require('../src/util/readWrite');
 
 if (!argv.outDir) {
   throw new Error('outDir argument is required');
@@ -19,26 +21,38 @@ async function generate() {
   const files = await fs.readdir(outDir);
   if (files.length > 0) throw new Error('outDir must be emtpy');
 
-  const image = await generateSymbolImage({
-    imageOptions: { fontSize: 24 }
+  const { image, chars } = await generateSymbolImage({
+    imageOptions: {
+      allowedRotation: 2,
+      numberPerLine: 3,
+      fontSize: 24
+    }
   });
-  const { lines } = getLinesFromImage(image);
-  // eslint-disable-next-line no-await-in-loop
+  const { lines } = getLinesFromImage(image, {
+    roiOptions,
+    fingerprintOptions: {
+      width: 18,
+      height: 18
+    }
+  });
+  let count = 0;
   for (let line of lines) {
-    console.log(line);
-    // for (let roi of line.rois) {
-    //   const img = image.crop({
-    //     x: roi.minX,
-    //     y: roi.minY,
-    //     width: roi.width,
-    //     height: roi.height
-    //   });
-    //   await writeImages({
-    //     image: img,
-    //     generated: true,
-    //     char
-    //   });
-    // }
+    // eslint-disable-next-line no-await-in-loop
+    for (let roi of line.rois) {
+      const img = image.crop({
+        x: roi.minX,
+        y: roi.minY,
+        width: roi.width,
+        height: roi.height
+      });
+      await writeImages({
+        filePath: path.join(outDir, `${chars[count]}-${count}.png`),
+        image: img,
+        generated: true,
+        char: chars[count]
+      });
+      count++;
+    }
   }
 }
 
