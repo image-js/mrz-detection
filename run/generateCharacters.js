8ab7950553866ae22fafa6fd25b38b7c9422aef1
Daniel Kostro <kostro.d@gmail.com>
2018-03-29 13:51:19 +0200

    dilate characters

--- run/generateCharacters.js
+++ run/generateCharacters.js
@@ -7,7 +7,9 @@ const fs = require('fs-extra');
 const { generateSymbolImage, getLinesFromImage } = require('ocr-tools');
 const minimist = require('minimist');
 
-const argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2), {
+  string: ['fontSizes', 'dilations']
+});
 const roiOptions = require('../src/roiOptions');
 const { writeImages } = require('../src/util/readWrite');
 
@@ -16,12 +18,17 @@ if (!argv.outDir) {
 }
 
 let outDir = path.resolve(argv.outDir);
-let fontSizes = [48];
+let dilations = [0];
+let fontSizes = [78];
 let maxRotation = 2;
-let rotations = 5;
+let rotations = 3;
 let fonts = ['ocrb'];
 if (argv.fontSizes) {
   fontSizes = argv.fontSizes.split(',').map((f) => +f);
+}
+
+if (argv.dilations) {
+  dilations = argv.dilations.split(',').map((d) => +d);
 }
 
 if (argv.fonts) {
@@ -34,47 +41,85 @@ async function generate() {
 
   let globalCount = 0;
   // grid over parameters
-  for (let fontSize of fontSizes) {
-    // eslint-disable-next-line no-await-in-loop
-    for (let font of fonts) {
-      console.log(font, fontSize);
-      const imageOptions = {
-        allowedRotation: maxRotation,
-        numberPerLine: rotations + (rotations + 1) % 2, // odd number to ensure 0 angle included
-        fontSize,
-        fontName: font
-      };
-      const { image, chars } = await generateSymbolImage(imageOptions);
-      const { lines } = getLinesFromImage(image, {
-        roiOptions: Object.assign({}, roiOptions, {
-          minRatio: undefined,
-          maxRatio: undefined
-        }),
-        fingerprintOptions: {
-          width: 18,
-          height: 18
-        }
-      });
-      checkExpectedRois(lines, imageOptions);
-      let count = 0;
-      for (let line of lines) {
-        // eslint-disable-next-line no-await-in-loop
-        for (let roi of line.rois) {
-          const img = image.crop({
-            x: roi.minX,
-            y: roi.minY,
-            width: roi.width,
-            height: roi.height
-          });
-          await writeImages({
-            filePath: path.join(outDir, `${chars[count]}-${globalCount}.png`),
-            image: img,
-            generated: true,
-            char: chars[count],
-            code: chars[count].charCodeAt(0)
-          });
-          globalCount++;
-          count++;
+  for (let kernelSize of dilations) {
+    for (let fontSize of fontSizes) {
+      // eslint-disable-next-line no-await-in-loop
+      for (let font of fonts) {
+        const imageOptions = {
+          allowedRotation: maxRotation,
+          numberPerLine: rotations + (rotations + 1) % 2, // odd number to ensure 0 angle included
+          fontSize,
+          fontName: font
+        };
+        let { image, chars } = await generateSymbolImage(imageOptions);
+        const { lines } = getLinesFromImage(image, {
+          roiOptions: Object.assign({}, roiOptions, {
+            minRatio: undefined,
+            maxRatio: undefined
+          }),
+          fingerprintOptions: {
+            width: 18,
+            height: 18
+          }
+        });
+        checkExpectedRois(lines, imageOptions);
+        let count = 0;
+        for (let line of lines) {
+          // eslint-disable-next-line no-await-in-loop
+          for (let roi of line.rois) {
+            const borderSize = (kernelSize - 1) / 2;
+            let img = image
+              .crop({
+                x: roi.minX,
+                y: roi.minY,
+                width: roi.width,
+                height: roi.height
+              })
+              .grey();
+
+            let mask;
+            let roiManager;
+            if (kernelSize) {
+              img = img
+                .pad({ size: borderSize, algorithm: 'set', color: [255] })
+                .dilate({ kernel: getKernel(kernelSize) });
+              mask = img.mask({ threshold: 0.99 });
+
+              roiManager = img.getRoiManager();
+              roiManager.fromMask(mask);
+              let rois = roiManager.getRois().filter((roi) => roi.minX > 0);
+              rois.forEach((roi) => {
+                let mask = roi.getMask();
+                let mbr = mask.minimalBoundingRectangle();
+                roi.mbrWidth = getDistance(mbr[0], mbr[1]);
+                roi.mbrHeight = getDistance(mbr[1], mbr[2]);
+                roi.mbrSurface = roi.mbrWidth * roi.mbrHeight;
+                roi.fillingFactor = roi.surface / roi.mbrSurface;
+              });
+
+              rois.sort(
+                (roiA, roiB) => roiA.fillingFactor - roiB.fillingFactor
+              );
+              img = img.crop({
+                x: rois[0].minX,
+                y: rois[0].minY,
+                width: rois[0].width,
+                height: rois[0].height
+              });
+            }
+
+            img = img.scale({ width: 18, height: 18 });
+
+            await writeImages({
+              filePath: path.join(outDir, `${chars[count]}-${globalCount}.png`),
+              image: img /*roiManager.paint()*/,
+              generated: true,
+              char: chars[count],
+              code: chars[count].charCodeAt(0)
+            });
+            globalCount++;
+            count++;
+          }
         }
       }
     }
@@ -84,8 +129,22 @@ async function generate() {
 function checkExpectedRois(lines, options) {
   assert.equal(lines.length, 37, 'correct number of lines');
   const nbRois = lines.reduce((prev, current) => prev + current.rois.length, 0);
-  console.log(nbRois);
   assert.equal(nbRois, options.numberPerLine * 37);
 }
 
 generate();
+
+function getKernel(size) {
+  const array = new Array(size);
+  for (let i = 0; i < size; i++) {
+    array[i] = new Array(size);
+    for (let j = 0; j < size; j++) {
+      array[i][j] = 1;
+    }
+  }
+  return array;
+}
+
+function getDistance(p1, p2) {
+  return Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
+}
