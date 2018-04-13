98ee5cc36e1e2526f8df885de6ade92d08065252
Daniel Kostro <kostro.d@gmail.com>
2018-04-13 13:58:22 +0200

    update libsvm-js and use wasm

--- src/svm.js
+++ src/svm.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const fs = require('fs-extra');
 const hog = require('hog-features');
-const SVMPromise = Promise.resolve(require('libsvm-js/asm'));
+const SVMPromise = Promise.resolve(require('libsvm-js/wasm'));
 const Kernel = require('ml-kernel');
 const range = require('lodash.range');
 const BSON = require('bson');
