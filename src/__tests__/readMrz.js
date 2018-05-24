36df3054bae007c776ea24d42c132b28b3935679
Daniel Kostro <kostro.d@gmail.com>
2018-05-24 11:21:31 +0200

    add second id test

--- src/__tests__/readMrz.js
+++ src/__tests__/readMrz.js
@@ -9,11 +9,22 @@ const { getMrz, readMrz } = require('../..');
 it(
   'test the extraction of MRZ characters on an identity card',
   async () => {
-    const img = await Image.load(path.join(__dirname, 'fixtures/id1.png'));
+    const img = await Image.load(path.join(__dirname, 'fixtures/id1.jpg'));
     let mrzImage = getMrz(img);
     // mrzImage.save('./test.jpg');
     let mrz = await readMrz(mrzImage);
     expect(mrz).toMatchSnapshot();
   },
-  10000
+  15000
+);
+
+it(
+  'test the extraction of MRZ characters on an identity card 2',
+  async () => {
+    const img = await Image.load(path.join(__dirname, 'fixtures/id2.jpg'));
+    let mrzImage = getMrz(img);
+    let mrz = await readMrz(mrzImage);
+    expect(mrz).toMatchSnapshot();
+  },
+  15000
 );
